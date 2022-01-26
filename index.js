b1de984892bb421237c0ddbe1e94327f4891b160
Dalya Sohl <dalyasohl@hotmail.com>
2022-01-26 00:15:58 -0500

    bonus start

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 let userName = window.prompt ('Please enter your name:')
 let userScore = 0
 
+function playGame() {
 for(let x = 0; x < questions.length; x++) {
     let question = questions[x]
     let userAnswer = window.prompt(question.text)
     if(userAnswer === question.correctAnswer){
 userScore = userScore + 10
     }
-    //console.log(questions[x])
 }
 window.alert('Your score is: '+userScore)
+}
+playGame()

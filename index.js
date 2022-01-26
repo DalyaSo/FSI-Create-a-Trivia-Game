8d8fafc1d91bed864b19b0bc51c0833b4b67c5ad
Dalya Sohl <dalyasohl@hotmail.com>
2022-01-26 00:04:51 -0500

    final changes before bonus

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your Code Here
+let userName = window.prompt ('Please enter your name:')
+let userScore = 0
+
+for(let x = 0; x < questions.length; x++) {
+    let question = questions[x]
+    let userAnswer = window.prompt(question.text)
+    if(userAnswer === question.correctAnswer){
+userScore = userScore + 10
+    }
+    //console.log(questions[x])
+}
+window.alert('Your score is: '+userScore)
